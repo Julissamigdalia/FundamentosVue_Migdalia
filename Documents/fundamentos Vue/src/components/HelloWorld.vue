de5bfa3fdb8a059b29ba987b3b40a91db7146b62
Julissamigdalia <julissamigdaliaj@gmail.com>
2025-03-17 21:54:15 -0600

    Agregado el componente HelloWorldVue

---
+++ Documents/fundamentos Vue/src/components/HelloWorld.vue
@@ -0,0 +1,29 @@
+<script setup>
+//metodos
+</script>
+
+<template>
+
+<h1>Hola Mundo</h1>
+<button class="btn">Hola soy un boton</button>
+<br><br>
+<img src="https://ciclovivo.com.br/wp-content/uploads/2018/10/iStock-536613027.jpg  " >
+<br><br>
+<p>Hola soy migdalia estudio ingenieria en sistemas computacionales en la ciudad de tlaxiaco</p>
+
+</template>
+
+<style scoped>
+/* estilos css */
+h1 {
+  font-family: Verdana, Geneva, Tahoma, sans-serif;
+  font-size: 45px;
+  color: red;
+}
+.btn{
+  border-radius: 15px;
+  background-color: #ffff;
+  color: black;
+  font-family: 'Gill Sans','Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+}
+</style>
